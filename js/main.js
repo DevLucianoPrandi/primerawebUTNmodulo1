7b4312c761ef2833a279cfa17c042fc339155199
DevLucianoPrandi <devlucianoprandi@gmail.com>
2022-10-18 08:23:56 -0300

    sube web1

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let nombre=prompt("ingrese su nombre");
+let apellido= prompt ("ingrese su apellido");
+
+let edad= parseInt(prompt("ingrese su edad"));
+let edadaum=100;
+alert ("hola "+ nombre +" "+ apellido)
